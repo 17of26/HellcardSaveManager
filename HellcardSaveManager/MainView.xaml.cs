420c0ce27f32fbfe2b8438d5d5a4742f7749d408
FleckiBunti <36922814+FleckiBunti@users.noreply.github.com>
2020-05-24 12:24:54 -0400

    Minor UI improvements

--- HellcardSaveManager/MainView.xaml.cs
+++ HellcardSaveManager/MainView.xaml.cs
@@ -6,10 +6,5 @@
         {
             InitializeComponent();
         }
-
-        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
-        }
     }
 }
