ccbac95a99a3e00988741286659b886e38e7f87e
Astrid / Essa <47655917+Essarielle@users.noreply.github.com>
2020-05-28 13:58:37 +0200

    send mail async

--- HellcardSaveManager/SendLog.xaml.cs
+++ HellcardSaveManager/SendLog.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 //using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.IO.Compression;
 using System.Net.Mail;
 using System.Text;
@@ -14,7 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.IO;
+
 
 namespace HellcardSaveManager
 {
@@ -27,7 +28,7 @@ namespace HellcardSaveManager
         private const string _logFile = "HELLCARD_Demo_log.txt";
         private const string _logsHistory = "logs";
         private const string _processName = "HELLCARD_Demo";
-        private const string _emailTo = "awesomehellcardtool@thingtrunk.com"; //change this for developing purposes as to not spam thingtrunk
+        private const string _emailTo = "Threnodia@gmx.de"; //"awesomehellcardtool@thingtrunk.com"; //change this for developing purposes as to not spam thingtrunk
         private const string _emailSubject = "Hellcard Log";
         private const string _smtpClient = "mail.funkfreunde.net"; //please don't abuse!
         private const string _smtpUser = "HellcardCommunityTool@funkfreunde.net";
@@ -72,17 +73,8 @@ namespace HellcardSaveManager
                 return;
             }
 
-            string zipFile;
-
-            //build email
-            using (MailMessage mail = new MailMessage())
-            {
-                mail.From = new MailAddress(_smtpUser);
-                mail.To.Add(new MailAddress(_emailTo));
-                mail.Subject = _emailSubject;
-                mail.IsBodyHtml = true;
-                mail.Body = "<html>Hello Thing Trunk team,<br/><br/>"
-                    + "this is an automatically created email with some attachment (might be: log, historical logs, dump file).<br/><br/>"
+            string htmlbody = "<html>Hello Thing Trunk team,<br/><br/>"
+                    + "this is an automatically created email with some attachments (might be: log, historical logs, dump files).<br/><br/>"
                     + "User: " + tbxName.Text + "<br/>"
                     + "Other players: " + tbxPartners.Text + "<br/>"
                     + "Issue description:<br/>" + tbxDescription.Text.Replace("\r\n", "<br/>") + "<br/><br/>"
@@ -90,55 +82,46 @@ namespace HellcardSaveManager
                     + "Your Hellcard Save Manager Community Team<br/><br/><br/>"
                     + "P.S.: If you ever get spammed by mails from this adress, please contact Essarielle @Discord.</html>";
 
-                //get attachments
-                //most recent logfile
-                mail.Attachments.Add(new Attachment(Logfile.FullName));
-                //historical logfiles
-                zipFile = System.IO.Path.Combine(Logfile.DirectoryName, "HistLogs_" + DateTime.Now.ToString("yyyyMMdd_HHmm") + ".zip");
-                ZipFile.CreateFromDirectory(System.IO.Path.Combine(Logfile.DirectoryName, _logsHistory), zipFile);
-                mail.Attachments.Add(new Attachment(zipFile));
-                //minidump file if isSendMinidump and try sending a crashdump.dmp
-                if (IsSendMinidump)
-                {
-                    var minidumpInfo = GameDir.EnumerateFiles("*.mdmp", SearchOption.TopDirectoryOnly).LastOrDefault();
-                    mail.Attachments.Add(new Attachment(minidumpInfo.FullName));
-                    var dumpInfo = GameDir.EnumerateFiles("crashdump.dmp", SearchOption.TopDirectoryOnly).LastOrDefault();
-                    if (dumpInfo != null) 
-                    { 
-                        mail.Attachments.Add(new Attachment(dumpInfo.FullName));
-                    }
-                }
 
+            //make list of attachments
+            List<string> lstAttachments = new List<string>();
+            List<bool> lstDeleteAttach = new List<bool>();
 
-                //set server and send mail
-                SmtpClient smtp = new SmtpClient(_smtpClient);
-                System.Net.NetworkCredential cred = new System.Net.NetworkCredential(_smtpUser, SimpleCrypt.Crypt.Decrypt(_smtpPWcrypt));
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = cred;
-                smtp.Port = 25;
-                smtp.EnableSsl = false;
-                smtp.Send(mail);
-            }
+            //most recent logfile
+            lstAttachments.Add(Logfile.FullName);
+            lstDeleteAttach.Add(false); //don't delete main log file
 
-            //delete temp files
-            System.IO.FileInfo fiZip = new System.IO.FileInfo(zipFile);
-            for (int i = 0;  i < 30; i++)
+            //historical logfiles
+            string zipFile = System.IO.Path.Combine(Logfile.DirectoryName, "HistLogs_" + DateTime.Now.ToString("yyyyMMdd_HHmm") + ".zip");
+            ZipFile.CreateFromDirectory(System.IO.Path.Combine(Logfile.DirectoryName, _logsHistory), zipFile);
+            lstAttachments.Add(zipFile);
+            lstDeleteAttach.Add(true); //delete zip of historical log files after send
+
+            //minidump file if isSendMinidump and try sending a crashdump.dmp
+            if (IsSendMinidump)
             {
-                try
+                var minidumpInfo = GameDir.EnumerateFiles("*.mdmp", SearchOption.TopDirectoryOnly).LastOrDefault();
+                lstAttachments.Add(minidumpInfo.FullName);
+                lstDeleteAttach.Add(false);
+
+                var dumpInfo = GameDir.EnumerateFiles("crashdump.dmp", SearchOption.TopDirectoryOnly).LastOrDefault();
+                if (dumpInfo != null)
                 {
-                    fiZip.Delete();
-                    break;
+                    lstAttachments.Add(dumpInfo.FullName);
+                    lstDeleteAttach.Add(false);
                 }
-                catch (System.IO.IOException)
-                {
-                    System.Threading.Thread.Sleep(200);
-                }                    
             }
-            
+
+            Task.Run(() =>
+            SendMail.SendMailSmtp(_emailTo, _emailSubject, htmlbody,
+                                    lstAttachments.ToArray(), lstDeleteAttach.ToArray(),
+                                    _smtpClient, _smtpUser, _smtpPWcrypt) );
 
             //close window
             Close();
         }
+
+
 
         //private void BtnTest_Click(object sender, RoutedEventArgs e)
         //{
---
+++ HellcardSaveManager/SendMail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace HellcardSaveManager
+{
+    static class SendMail
+    {
+        public static void SendMailSmtp(string to,
+                                        string subject,
+                                        string htmlbody,
+                                        string[] arrAttachements,
+                                        bool[] arrDeleteAttach,
+                                        string smtpclient,
+                                        string smtpuser,
+                                        byte[] smtpPWcrypt                                        )
+        {
+            //build email
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(smtpuser);
+                mail.To.Add(new MailAddress(to));
+                mail.Subject = subject;
+                mail.IsBodyHtml = true;
+                mail.Body = htmlbody;
+
+                foreach (string attachment in arrAttachements)
+                {
+                    mail.Attachments.Add(new Attachment(attachment));
+                }
+
+
+                //set server and send mail
+                SmtpClient smtp = new SmtpClient(smtpclient);
+                System.Net.NetworkCredential cred = new System.Net.NetworkCredential(smtpuser, SimpleCrypt.Crypt.Decrypt(smtpPWcrypt));
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = cred;
+                smtp.Port = 25;
+                smtp.EnableSsl = false;
+                smtp.Send(mail);
+            }
+
+            //delete temp files
+            System.Threading.Thread.Sleep(1000); //wait 1sec, hopefully, no files are in use now
+
+            for (int i = 0; i < arrAttachements.Length; i++)
+            {
+                if (arrDeleteAttach[i])
+                {
+                    System.IO.FileInfo fily = new System.IO.FileInfo(arrAttachements[i]);
+                    fily.Delete();
+                }
+            }
+
+            
+        }
+
+
+    }
+}
