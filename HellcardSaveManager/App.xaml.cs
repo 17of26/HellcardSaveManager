6409dadf4fda0082f79700fd19b4f5d404279014
Astrid / Essa <47655917+Essarielle@users.noreply.github.com>
2020-05-28 10:48:48 +0200

    error handling + mail text
    * app wide error handling (not in debug!)
    * updated the "email contains" part in window and email text itself (dumpfile)

--- HellcardSaveManager/App.xaml.cs
+++ HellcardSaveManager/App.xaml.cs
@@ -9,5 +9,14 @@ namespace HellcardSaveManager
             var window = new MainView {DataContext = new MainVm()};
             window.Show();
         }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Please seek help in the Hellcard Discord Channel and mention following error:\n\n"
+                     + e.Exception.GetType().ToString() + ": " + e.Exception.Message + "\n\n\n"
+                     + "(This tool was not created by nor is supported by Thing Trunk, it's a community project.)", "Error");
+
+            e.Handled = true;
+        }
     }
 }
